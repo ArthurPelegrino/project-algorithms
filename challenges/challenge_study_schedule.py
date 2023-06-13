f3cc375e3bc89ff8dcaaa0b4a03d4241c7504efa
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-06-13 12:35:03 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # permanence_period = tuplas repesentando tempo de permanencia [(2,4)...]
+    x = 0
+    if target_time is None:
+        return None
+        # for number in target_time:
+    for time_in in permanence_period:
+        if not isinstance(time_in[0], int) or not isinstance(time_in[1], int):
+            return None
+        if target_time >= time_in[0] and target_time <= time_in[1]:
+            x += 1
+    return x
