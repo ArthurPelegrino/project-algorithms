21c4dde92b4c926d214c6b08bf1e66eedf9924a2
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-06-14 09:06:24 -0300

    Requisito 2 - testes

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,13 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    pass
+    # first_string.lower()
+    # second_string.lower()
+    # if first_string == "" or second_string == "":
+    #     return first_string, second_string, False
+
+
+# 'arthur'
+# middle = len('arthur') // 2
+#  middle = 3
+# left = 'arth': ur
+# right =
