70bb01adc19533d5f050432e84b232aef993dc7a
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-06-14 11:59:22 -0300

    Requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # print("numeros param", nums)
+    nums.sort()
+    # print("numbers sort", nums)
+    if nums is None or nums is str:
+        return False
+    else:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                if nums[i] < 0:
+                    return False
+                else:
+                    return nums[i]
+    return False
+
+
+# print(find_duplicate([1, 2, 3]))
