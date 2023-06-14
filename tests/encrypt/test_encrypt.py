b55b83d43652bf870dd476ca764acb0a62d32103
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-06-14 09:23:43 -0300

    Requisito 2 - testes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,26 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
 
 
-def pt_message():
-    pass
+def test_encrypt_message():
+    try:
+        encrypt_message("2", "a")
+    except TypeError as error:
+        assert str(error) == "tipo inválido para key"
+    try:
+        encrypt_message(2, 2)
+    except TypeError as error:
+        assert str(error) == "tipo inválido para message"
+        assert encrypt_message("abcd", 4) == "".join(reversed("abcd"))
+
+
+def test_encrypt_message_incorrect_return():
+    right_odd = encrypt_message("abcde", 3)
+    even_odd = encrypt_message("abcde", 2)
+    if right_odd == "edc_ba":
+        raise AssertionError("unnexpected result 'edc_ba'")
+    if even_odd == "cba_ed":
+        raise AssertionError("unnexpected result 'cba_ed'")
+
+    # encrypt_message("abcde", 3) == "cba_ed" // resultado correto com ímpar
+
+    # assert encrypt_message("abcde", 2) == "edc_ba" // resultado corre com par
