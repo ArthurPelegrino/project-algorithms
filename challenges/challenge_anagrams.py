5a81d1ae2fc0772988699fea3a7ed2a37f2e502b
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-06-14 09:36:46 -0300

    Tentando requisito 2 novamente

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,10 +4,12 @@ def is_anagram(first_string, second_string):
     # second_string.lower()
     # if first_string == "" or second_string == "":
     #     return first_string, second_string, False
+    # if len(first_string) is not len(second_string):
+    #     return first_string, second_string, False
 
 
-# 'arthur'
-# middle = len('arthur') // 2
-#  middle = 3
-# left = 'arth': ur
-# right =
+# devo receber duas strings
+# ordenar essas strings por ordem alfabética (?)
+# retornar uma tupla (string_ordenada, string_ordenada,
+#  booleano_se_é_ou_não_um_anagrama)
+#
