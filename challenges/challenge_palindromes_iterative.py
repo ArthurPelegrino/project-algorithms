0e1b79c798a13891c1419fed4b334b23747c754a
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-06-14 13:30:10 -0300

    Requisito 6 e aprovação! VQV!

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    comparision_word = word[::-1]
+    if len(word) == 1:
+        return True
+    if word is None or word == "":
+        return False
+    if word == comparision_word:
+        return True
+    else:
+        return False
